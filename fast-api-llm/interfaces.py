655dfe2131b614f397ee40643817cc65d2944dce
Yash Jain NUID(002871982) <145181114+yashjainNEU@users.noreply.github.com>
2024-04-13 13:31:34 -0400

    interface for train and query

--- fast-api-llm/interfaces.py
+++ fast-api-llm/interfaces.py
@@ -3,3 +3,10 @@ from pydantic import BaseModel
 
 class Query(BaseModel):
     query: str
+    user: str
+    model_name:  str = "default"
+
+
+class TrainBody(BaseModel):
+    model_key:str
+    file_path:str
